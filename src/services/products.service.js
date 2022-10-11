98a99d270633f486dbdb828cce54c5e70dc0d181
Lauro Pereira <lauropera01@gmail.com>
2022-10-11 17:30:52 -0300

    Adiciona arquivo de service  dos products

---
+++ src/services/products.service.js
@@ -0,0 +1,17 @@
+const productsModel = require('../models/products.model');
+
+const findAll = async () => {
+  const products = await productsModel.findAll();
+  return { type: null, message: products };
+};
+
+const findById = async (productId) => {
+  const product = await productsModel.findById(productId);
+  if (product) return { type: null, message: product };
+  return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
