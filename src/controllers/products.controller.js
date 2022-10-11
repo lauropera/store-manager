0b10a3e21dd2e9a3ba894ad63c99d22ed626c8ad
Lauro Pereira <lauropera01@gmail.com>
2022-10-11 17:32:10 -0300

    Adiciona arquivo de controller dos products e a util errorMap

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,19 @@
+const productsService = require('../services/products.service');
+const errorMap = require('../utils/errorMap');
+
+const listProducts = async (_req, res) => {
+  const { message } = await productsService.findAll();
+  res.status(200).json(message);
+};
+
+const getProduct = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productsService.findById(id);
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+  res.status(200).json(message);
+};
+
+module.exports = {
+  listProducts,
+  getProduct,
+};
